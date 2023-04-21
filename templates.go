46e1f2ed54b1ea9ae17788af2623ced14e7f59a2
David Rio Deiros <driodeiros@gmail.com>
2023-04-21 16:18:15 -0400

    Move styles to dedicated file

--- templates.go
+++ templates.go
@@ -6,65 +6,8 @@ var homeTmpl = `<!DOCTYPE html>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Keep an eye (home)</title>
   <link rel="icon" type="image/x-icon" href="/assets/favicon-32x32.png">
-
-  <style>
-  body {
-    font-size: 1rem;
-    padding: 2rem;
-    background-color: rgb(246 244 242/0.7);
-    font-family: Inter,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;
-  }
-  h1 {
-    padding: .1rem 0 .1rem 0;
-    margin: .1rem 0 .1rem 0;
-  }
-  a {
-    text-decoration: none;
-    color: steelblue;
-  }
-  .danger {
-    color: red;
-  }
-
-  form > input {
-    margin-top: .5rem;
-  }
-
-  form > button {
-    margin-top: .5rem;
-  }
-
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-
-  li {
-    margin: 0 0 1rem 0;
-    padding: 0 0 .5rem 0;
-  }
-
-  .tokens-container {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    padding: 1rem 0 0 0;
-  }
-
-  .entry {
-    display: flex;
-    flex-direction: column;
-    width: 200px;
-    margin: 0.5rem;
-    padding: 0.4rem;
-    min-height: 100px;
-    border: solid 1px silver;
-    border-radius: 5px;
-  }
-
-  </style>
-
- </head>
+  <link rel="stylesheet" href="/assets/style.css">
+  </head>
 <body style="padding: 1rem">
 
   <h1>Keep an Eye</h1>
