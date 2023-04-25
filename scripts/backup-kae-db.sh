3a062838f28921a8aa016266de39f470c3197e97
David Rio Deiros <driodeiros@gmail.com>
2023-04-25 08:13:39 -0400

    update air

--- scripts/backup-kae-db.sh
+++ scripts/backup-kae-db.sh
@@ -7,5 +7,5 @@ gzip /tmp/db.kae
 
 aws s3 cp /tmp/db.kae.gz s3://drio-kae-backup/backup-`date +%d%H`.gz
 
-curl https://kae.driohq.net/kb/vzndxvgbtlzqlkjrfkkz &> /dev/null
+curl https://kae.driohq.net/hb/vzndxvgbtlzqlkjrfkkz &> /dev/null
 rm -f /tmp/db.kae /tmp/db.kae.gz
